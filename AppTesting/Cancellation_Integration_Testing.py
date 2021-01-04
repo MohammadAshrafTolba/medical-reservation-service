3f14d292f7ab326528829563baeef2134476cbd6
mharb98 <marwanharb65@outlook.com>
2021-01-04 13:46:03 +0200

    Fixed something in test case

--- AppTesting/Cancellation_Integration_Testing.py
+++ AppTesting/Cancellation_Integration_Testing.py
@@ -2,11 +2,25 @@ from flask import Flask
 import json
 from app.routes import cancel_appointment
 from flaskr import flaskr
+import pytest
+
+@pytest.fixture
+def client():
+    db_fd, flaskr.app.config['routes'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_app()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(flaskr.app.config['routes'])
 
 def test_cancel_valid_appointment():
     app = Flask(__name__)
     handler = new PatientAppointmentHandler()
-    client = app.test_client()
+    client = flaskr.app.test_client()
     url = '/cancel_appointment'
      mock_request_data = {
         'appointment_id' : 1
